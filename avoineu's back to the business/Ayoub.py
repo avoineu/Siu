a2d467119cd54c9226fa3b1eccfee94c55767cd9
Ayoub Ben Yamoune <21160@ecam.be>
2023-03-20 11:05:00 +0100

    download the requirements

---
+++ avoineu's back to the business/Ayoub.py
@@ -0,0 +1,17 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host, port =('localhost', 6666)
+
+try :
+    s.connect((host,port))
+    print("le client arrive...")
+
+    data = 'ca marche le reuf'
+    data = data.encode("utf8")
+    s.sendall(data)
+
+except ConnectionRefusedError :
+    print("ERREUR")
+
+finally :
+    s.close()
