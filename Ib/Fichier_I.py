4642a6b73df583d38e6d6acd544af2592397c54e
Mister-I.Z <20057@ecam.be>
2023-03-28 19:39:41 +0200

    Code client OK

--- Ib/Fichier_I.py
+++ Ib/Fichier_I.py
@@ -1,31 +1,44 @@
-import threading
 import socket
 import json
+import time
 
-s = socket.socket()
-serveradress = ('localhost', 3000)
+server_address = ('localhost', 3000)
 
+# Création de la requête de souscription
 request = {
-   "request": "subscribe",
-   "port": 8888,
-   "name": "fun_name_for_the_client",
-   "matricules": ["12345", "67890"]
+    "request": "subscribe",
+    "port": 8888,
+    "name": "fun_name_for_the_client",
+    "matricules": ["12345", "67890"]
 }
 
-with socket.socket() as s:
-    s.connect(serveradress)
-    s.send(json.dumps(request).encode())
-    response = s.recv(2048).decode()
+# Création de la socket et envoi de la requête de souscription au serveur
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(server_address)
+    s.sendall(json.dumps(request).encode())
+    response = s.recv(1024).decode()
+    print(response)
 
-print(response)
-
-with socket.socket() as s:
-    s.bind('localhost', 3000)
+# Création de la socket et écoute sur le port de souscription
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('localhost', 8888))
     s.listen()
-    while True : 
-        try :
-            client, adress = s.accept()
-        except :
 
-        response = s.recv(2048).decode()
-    
+    while True:
+        # Acceptation de la connexion entrante
+        client_socket, client_address = s.accept()
+        print('Connexion de', client_address)
+
+        # Réception du message envoyé par le serveur
+        data = client_socket.recv(1024).decode()
+        print('Reçu', repr(data))
+
+        # Analyse du message reçu et envoi de la réponse appropriée
+        message = json.loads(data)
+        if message['request'] == 'ping':
+            response = {"response": "pong"}
+            print(response)
+            client_socket.sendall(json.dumps(response).encode())
+
+        # Fermeture de la connexion
+        client_socket.close()
