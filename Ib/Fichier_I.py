741bf23fea5b453324cead265eec54c433be80ea
Mister-I.Z <20057@ecam.be>
2023-03-29 09:10:21 +0200

    Qlq modif sur le code

--- Ib/Fichier_I.py
+++ Ib/Fichier_I.py
@@ -3,18 +3,19 @@ import json
 import time
 
 server_address = ('localhost', 3000)
+Variable = True
 
 # Création de la requête de souscription
 request = {
     "request": "subscribe",
     "port": 8888,
-    "name": "fun_name_for_the_client",
-    "matricules": ["12345", "67890"]
+    "name": "Avoine and Mister_IZ",
+    "matricules": ["21160", "20057"]
 }
 
 # Création de la socket et envoi de la requête de souscription au serveur
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.settimeout(0.5)
+    s.settimeout(5)
     try:
         s.connect(server_address)
         s.sendall(json.dumps(request).encode())
@@ -22,29 +23,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(response)
     except socket.timeout:
         print("Le temps d'attente pour la connexion est trop long !")
+        pass
 
 # Création de la socket et écoute sur le port de souscription
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('localhost', 8888))
     s.listen()
 
-    while True:
-        # Acceptation de la connexion entrante
-        client_socket, client_address = s.accept()
-        print('Connexion de', client_address)
+    while Variable:
+        s.settimeout(5)
+            
+        try: 
+            # Acceptation de la connexion entrante
+            client_socket, client_address = s.accept()
+            print('Connexion de', client_address)
 
-        # Réception du message envoyé par le serveur
-        data = client_socket.recv(1024).decode()
-        print('Reçu', repr(data))
+            # Réception du message envoyé par le serveur
+            data = client_socket.recv(1024).decode()
+            print('Reçu', repr(data))
 
-        # Analyse du message reçu et envoi de la réponse appropriée
-        message = json.loads(data)
-        if message['request'] == 'ping':
-            response = {"response": "pong"}
-            print(response)
-            client_socket.sendall(json.dumps(response).encode())
+            # Analyse du message reçu et envoi de la réponse appropriée
+            message = json.loads(data)
+            if message['request'] == 'ping':
+                response = {"response": "pong"}
+                print(response)
+                client_socket.sendall(json.dumps(response).encode())
 
-        # Fermeture de la connexion
-        client_socket.close()
+            # Fermeture de la connexion
+            client_socket.close()
+        except:
+            print("Trop long pour l'accept")
+            pass
 
-        break #pcq la boucle ne veut pas s'arrêter avec ctrl+c. Pq ?
+        Variable = False #Pour arrêter la boucle étant donné qu'on est déja accepé
