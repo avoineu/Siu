81171ab1f7fac857f2cd849603a051b02cf17d9c
Ayoub Ben Yamoune <21160@ecam.be>
2023-04-19 10:14:33 +0200

    probleme envoie de tile

--- ay/Fichier_A.py
+++ ay/Fichier_A.py
@@ -41,6 +41,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Acceptation de la connexion entrante
             client_socket, client_address = s.accept()
             with client_socket:
+                def reponse() :
+                    chiffre = random.randint(6,8)
+                    toile = message['tile']
+                    toile[0]= message['tile'][3]
+                    toile[1]= message['tile'][0]
+                    toile[2]= message['tile'][1]
+                    toile[3]= message['tile'][2]
+                    jouer = {"tile":toile, "gate":"B", "new_position":chiffre}
+                    aenvoyer = {"response":"move", "move":jouer, "message":"kuku"}
+                    client_socket.sendall(json.dumps(aenvoyer).encode())
+
                 print('Connexion de', client_address)
 
                 # Réception du message envoyé par le serveur
@@ -53,13 +64,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     response = {"response": "pong"}
                     print(response)
                     client_socket.sendall(json.dumps(response).encode())
+                elif message['request'] == 'play':
+                    reponse()
 
         except socket.timeout:
             pass
 
         #Variable = False #Pour arrêter la boucle étant donné qu'on est déja accepté
-
-        if data :
-            chiffre = random.randint(6,8)
-            jouer = {"title":"N","gate":"A" ,"new_position": chiffre}
-            client_socket.sendall(json.dumps(jouer).encode())
